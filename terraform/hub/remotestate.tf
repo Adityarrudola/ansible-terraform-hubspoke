0ad773b2dae73efc4177c5af5d39f5cf9886ef18
Aditya Rudola <adityarrudola@gmail.com>
2026-03-07 16:33:58 +0530

    changes in remotestate file

--- terraform/hub/remotestate.tf
+++ terraform/hub/remotestate.tf
@@ -2,9 +2,9 @@ data "terraform_remote_state" "dev2" {
   backend = "azurerm"
 
   config = {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstateaditya"
-    container_name       = "tfstate"
-    key                  = "dev2/terraform.tfstate"
+    resource_group_name  = "terraform-rg"
+    storage_account_name = "rterraformremotebackend"
+    container_name       = "ansible-terraform-state"
+    key                  = "dev-2/infra.tfstate"
   }
 }
