66f3018020365a230e6a6ca6b5eb0433044b0425
Aditya Rudola <adityarrudola@gmail.com>
2026-03-07 18:30:43 +0530

    acr .tf changes

--- terraform/hub/containerregistry.tf
+++ terraform/hub/containerregistry.tf
@@ -5,9 +5,3 @@ resource "azurerm_container_registry" "demo" {
   sku                 = "Basic"
   admin_enabled       = true
 }
-
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.demo.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_virtual_machine.demo.identity[0].principal_id
-}
