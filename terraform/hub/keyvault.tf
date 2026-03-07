72d330658d5fdf2c3e188f6d33b312abf37abb65
Aditya Rudola <adityarrudola@gmail.com>
2026-03-07 16:54:14 +0530

    keyvault access policy changes

--- terraform/hub/keyvault.tf
+++ terraform/hub/keyvault.tf
@@ -7,28 +7,50 @@ resource "azurerm_key_vault" "demo" {
   tenant_id           = data.azurerm_client_config.demo.tenant_id
 
   sku_name = "standard"
-
   purge_protection_enabled = false
+}
+
+resource "azurerm_key_vault_access_policy" "dev2_vm_policy" {
+  key_vault_id = azurerm_key_vault.demo.id
+  tenant_id    = data.azurerm_client_config.demo.tenant_id
+  object_id    = data.terraform_remote_state.dev2.outputs.dev2_vm_identity
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "jenkins_policy" {
+
+  key_vault_id = azurerm_key_vault.demo.id
+  tenant_id    = data.azurerm_client_config.demo.tenant_id
+  object_id    = data.azurerm_client_config.demo.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
 }
 
 resource "azurerm_key_vault_secret" "sql_username" {
   name         = "sql-username"
   value        = random_string.sql_username.result
   key_vault_id = azurerm_key_vault.demo.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.jenkins_policy
+  ]
 }
 
 resource "azurerm_key_vault_secret" "sql_password" {
   name         = "sql-password"
   value        = random_password.sql_password.result
   key_vault_id = azurerm_key_vault.demo.id
-}
 
-resource "azurerm_key_vault_access_policy" "dev2_vm_policy" {
-  key_vault_id = azurerm_key_vault.demo.id
-  tenant_id = data.azurerm_client_config.demo.tenant_id
-  object_id = data.terraform_remote_state.dev2.outputs.dev2_vm_identity
-  secret_permissions = [
-    "Get",
-    "List"
+  depends_on = [
+    azurerm_key_vault_access_policy.jenkins_policy
   ]
 }
