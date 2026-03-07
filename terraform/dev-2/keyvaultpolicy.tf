feaec90515907f8957cc68f3223b0605b0164d65
Aditya Rudola <adityarrudola@gmail.com>
2026-03-07 18:51:26 +0530

    keyvault .tf changes

--- terraform/dev-2/keyvaultpolicy.tf
+++ terraform/dev-2/keyvaultpolicy.tf
@@ -1,10 +1,14 @@
+data "azurerm_client_config" "demo" {}
+
 resource "azurerm_key_vault_access_policy" "dev2_vm_policy" {
-  key_vault_id = azurerm_key_vault.demo.id
+
+  key_vault_id = data.terraform_remote_state.hub.outputs.keyvault_id
   tenant_id    = data.azurerm_client_config.demo.tenant_id
-  object_id    = data.terraform_remote_state.dev2.outputs.dev2_vm_identity
+  object_id    = azurerm_linux_virtual_machine.demo.identity[0].principal_id
 
   secret_permissions = [
     "Get",
     "List"
   ]
+
 }
