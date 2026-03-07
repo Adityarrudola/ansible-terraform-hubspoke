8c0cb09eecad74a0620b2dbf4b40c4ce0fc57766
Aditya Rudola <adityarrudola@gmail.com>
2026-03-07 18:34:34 +0530

    acr .tf changes

--- terraform/dev-2/acr-role.tf
+++ terraform/dev-2/acr-role.tf
@@ -1,16 +1,9 @@
-data "terraform_remote_state" "hub" {
-  backend = "azurerm"
-
-  config = {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "rterraformremotebackend"
-    container_name       = "ansible-terraform-state"
-    key                  = "hub/infra.tfstate"
-  }
-}
-
 resource "azurerm_role_assignment" "acr_pull" {
   scope                = data.terraform_remote_state.hub.outputs.acr_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_linux_virtual_machine.demo.identity[0].principal_id
+
+  depends_on = [
+    azurerm_linux_virtual_machine.demo
+  ]
 }
