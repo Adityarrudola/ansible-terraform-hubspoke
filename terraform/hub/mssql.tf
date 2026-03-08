d79fa03ddb874f1fedf83a8d81f7f0840c9c8355
Aditya Rudola <adityarrudola@gmail.com>
2026-03-08 11:30:04 +0530

    final changes

--- terraform/hub/mssql.tf
+++ terraform/hub/mssql.tf
@@ -17,3 +17,10 @@ resource "azurerm_mssql_database" "demo" {
     name = var.env
   }
 }
+
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.demo.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
